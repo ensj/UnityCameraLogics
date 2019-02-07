c814c17ed673a0c7ad43ebfb1990f119f61c2842
ensj <ensjtrans@gmail.com>
2019-02-06 23:19:36 -0800

    Uploaded First Camera Logic
    Used Camera Window logic with x-axis.

---
+++ CameraPos.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPos : MonoBehaviour
+{
+    GameObject player;
+    PlayerMovement script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        script = player.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.x > transform.position.x + 3)
+        {
+            if (transform.position.x < 57)
+            {
+                transform.Translate(new Vector2(player.transform.position.x - transform.position.x - 3,0));
+            }
+        }else if(player.transform.position.x < transform.position.x - 3)
+        {
+            if (transform.position.x > 0)
+            {
+                transform.Translate(new Vector2(player.transform.position.x - transform.position.x + 3, 0));
+            }
+        }
+    }
+}
